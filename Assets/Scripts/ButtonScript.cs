79e495e2e01ac8de58b4627a78dc1b6b1b4a1282
ChristopherGug <cgugelmeier@gmail.com>
2020-10-04 21:21:41 -0400

    first commit

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        if (this.gameObject.name == "TestButton")
+            SceneManager.LoadScene("GameOver");
+        else if (this.gameObject.name == "StartButton")
+            SceneManager.LoadScene("GameScene");
+        else if (this.gameObject.name == "InstructionsButton")
+            SceneManager.LoadScene("InstructionsScene");
+        else if (this.gameObject.name == "BackButton")
+            SceneManager.LoadScene("MenuScreen");
+        else if (this.gameObject.name == "PlayAgainButton")
+            SceneManager.LoadScene("GameScene");
+    }
+
+
+}
